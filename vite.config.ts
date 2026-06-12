1eadedb5028b04eb3bf42c8b0439e70e9e5e722c
jjw <js48jjw@daum.net>
2026-06-12 14:49:42 +0900

    adsterra

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+    host: '127.0.0.1',
+  },
   plugins: [
     react(),
     VitePWA({
